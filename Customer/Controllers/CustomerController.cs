e32867f4df10180a7906d49cc6c8e4130826dc80
Ayşe Parla Yerli <parla.yerli@demirorenteknoloji.com>
2020-09-15 15:27:24 +0300

    Add project files.

---
+++ Customer/Controllers/CustomerController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Customer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "FirmaA", "FirmaB", "FirmaC", "FirmaD", "FirmaE", };
+        }
+    }
+}
---
+++ Product/Controllers/ProductController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Product.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "Uzman1", "Uzman2", "Uzman3", "Uzman4", "Uzman5", };
+        }
+
+        [HttpGet("{id}")]
+        public int GetById(int id)
+        {
+            return id;
+        }
+
+
+    }
+}
